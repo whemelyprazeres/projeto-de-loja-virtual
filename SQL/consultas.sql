85ff55524a45c54f43c36c62c7156d37ec4b9ea5
whemelyprazeres <0000113800800xsp@al.educacao.sp.gov.br>
2026-05-28 08:22:00 -0300

    Update consultas.sql
    adicionando consultas sql

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -1,3 +1,16 @@
+SELECT COUNT(*) FROM clientes;
 SELECT * FROM clientes;
 
-SELECT COUNT(*) FROM clientes;
+SELECT * FROM photocards;
+
+SELECT * FROM categorias;
+
+SELECT * FROM pedidos;
+
+SELECT COUNT(*) AS total_clientes
+FROM clientes;
+
+SELECT nome,preco
+FROM photocards
+WHERE preco > 20;SELECT nome, preco
+FROM photocards;
